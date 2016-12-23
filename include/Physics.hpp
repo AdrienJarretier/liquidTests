b685c881befa935fac0e73c5f79f68b3579d3260
Jarretier Adrien <jarretier.adrien@gmail.com>
2016-12-23 18:26:40 +0100

    consts are static and public

--- include/Physics.hpp
+++ include/Physics.hpp
@@ -16,20 +16,21 @@ class Physics
 public:
     static std::string toStr(const b2Vec2& vec2);
 
+    static constexpr float32 TIME_STEP = 1.0f / 60.0f;
+
+    static const int32 VELOCITY_ITERATIONS = 8;
+    static const int32 POSITION_ITERATIONS = 3;
+
     Physics();
     const b2World& getWorld() const;
 
     void step();
+
 //    const b2Vec2& getGroundPos();
 protected:
 private:
     b2Vec2 gravity;
     b2World world;
-
-    const float32 TIME_STEP = 1.0f / 60.0f;
-
-    const int32 VELOCITY_ITERATIONS = 8;
-    const int32 POSITION_ITERATIONS = 3;
 
 };
 
